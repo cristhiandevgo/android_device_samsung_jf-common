c62bb3aa42b6362b611d9de066d9d7d5034c7f8d
Vlad <tsvetkov_vlad@ukr.net>
2017-10-12 21:12:13 +0300

    move to legacy camera provider hidl

--- jf-common.mk
+++ jf-common.mk
@@ -95,7 +95,7 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-impl-legacy \
     camera.device@1.0-impl-legacy \
     camera.msm8960 \
     Snap
