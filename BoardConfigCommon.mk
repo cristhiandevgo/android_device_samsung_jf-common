a9d2581963527cc70475a2e9d3ad02b71fb671a1
Vlad <tsvetkov_vlad@ukr.net>
2017-10-18 19:02:27 +0300

    add BOARD_KERNEL_IMAGE_NAME

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -42,6 +42,7 @@ TARGET_BOOTLOADER_BOARD_NAME := MSM8960
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 zcache msm_rtb.filter=0x3F ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80200000
+BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 BOARD_KERNEL_PAGESIZE := 2048
 LZMA_RAMDISK_TARGETS := recovery
