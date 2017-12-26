15c54713bf0dd87d6d268e3c55140f9eed8494cd
Vlad <tsvetkov_vlad@ukr.net>
2017-12-27 00:28:40 +0200

    jf: Remove themald-8064
    and rename our config

--- jf-common.mk
+++ jf-common.mk
@@ -202,8 +202,7 @@ PRODUCT_COPY_FILES += \
 
 # Thermal
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermald-8064.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermald-8064.conf \
-    $(LOCAL_PATH)/configs/thermald-8064ab.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermald-8064ab.conf
+    $(LOCAL_PATH)/configs/thermald-8064ab.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermald.conf
 
 # Wifi
 PRODUCT_PACKAGES += \
