d2e76a1bc9669bd952c73f725197f53ea12820a1
Vlad <tsvetkov_vlad@ukr.net>
2017-12-30 22:56:21 +0200

    Revert "jf: Enable boot and system server dex-preopt"
    This reverts commit d94733ff38dec6d6825fb0e20a04035e4dc19a3c.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -85,16 +85,6 @@ BOARD_CHARGER_ENABLE_SUSPEND := true
 # LineageHW
 BOARD_HARDWARE_CLASS += $(COMMON_PATH)/lineagehw
 
-# Dexpreopt
-ifeq ($(HOST_OS),linux)
-  ifneq ($(TARGET_BUILD_VARIANT),eng)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
-    endif
-  endif
-endif
-
 # Display
 SF_START_GRAPHICS_ALLOCATOR_SERVICE := true
 NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
