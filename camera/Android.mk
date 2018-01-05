34c380fc70d92045fe2c94a399ec7b7e24a87903
Vlad <tsvetkov_vlad@ukr.net>
2018-01-05 19:51:43 +0200

    camera: Some rework of makefile

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,22 +1,35 @@
+ifeq ($(TARGET_PROVIDES_CAMERA_HAL),true)
+
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_C_INCLUDES += \
+    framework/native/include \
     system/media/camera/include
 
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils libcutils libdl libgui \
+    libhardware \
+    liblog \
+    libcamera_client \
+    libutils \
+    libcutils \
+    libdl \
+    libgui \
     android.hidl.token@1.0-utils \
     android.hardware.graphics.bufferqueue@1.0
 
-LOCAL_STATIC_LIBRARIES := libarect libbase
+LOCAL_STATIC_LIBRARIES := \
+    libarect \
+    libbase
 
 LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_MODULE := camera.msm8960
+LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
 LOCAL_PROPRIETARY_MODULE := true
 
 include $(BUILD_SHARED_LIBRARY)
+
+endif
