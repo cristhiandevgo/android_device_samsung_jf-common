7a05237b05f1c86675eacda8a4baffcb387d042d
Vlad <tsvetkov_vlad@ukr.net>
2018-01-17 18:50:10 +0200

    jf: Include legacy qcom sepolicy rules
    Change-Id: Ic499de98c34a63de18e935d2cc6f23f0e69b1a43

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -145,6 +145,7 @@ BOARD_PROVIDES_LIBRIL := true
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/legacy-sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
 
 
