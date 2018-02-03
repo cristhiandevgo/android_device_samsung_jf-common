6bb64866157ea5d3e2add4be4f83f44d862c5dc0
Vlad <tsvetkov_vlad@ukr.net>
2018-02-04 01:24:18 +0200

    jf: Remove unneeded spn-conf

--- jf-common.mk
+++ jf-common.mk
@@ -203,10 +203,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     $(LOCAL_PATH)/seccomp/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
-# SPN override
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
-
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermald-8064ab.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermald.conf
