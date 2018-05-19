29e9c6783adb9d571c0cb6031e902d20e292a239
Vlad <tsvetkov_vlad@ukr.net>
2018-05-19 16:39:43 +0300

    jf: Declare the arch and cpu variants
    originally posted here :Buff99/oreo_samsung_jf-common@588c57b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -36,6 +36,10 @@ TARGET_SPECIFIC_HEADER_PATH += device/samsung/jf-common/include
 TARGET_BOARD_PLATFORM := msm8960
 
 # Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := krait
 
 # Bootloader
