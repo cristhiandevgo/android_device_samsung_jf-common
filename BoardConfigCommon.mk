3a929b3df8c7282a9f4f4d805a85aae98bb8a1fe
Vlad <tsvetkov_vlad@ukr.net>
2018-05-25 20:31:42 +0300

    Revert "ALLOW_MISSING_DEPENDENCIES"
    This reverts commit 125b454aec63141cdb35f84ad81dd4ccd116bf52.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -157,5 +157,3 @@ WIFI_BAND := 802_11_ABG
 WIFI_DRIVER_FW_PATH_AP := "/vendor/etc/wifi/bcmdhd_apsta.bin"
 WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/dhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_STA := "/vendor/etc/wifi/bcmdhd_sta.bin"
-
-ALLOW_MISSING_DEPENDENCIES := true
