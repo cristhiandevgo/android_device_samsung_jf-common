3e3bbddc4b901aa6e7baf94b3e01da479783501b
Vlad <tsvetkov_vlad@ukr.net>
2018-07-05 16:23:37 +0300

    camera: temp revert to old wrapper

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,33 +1,22 @@
-ifeq ($(TARGET_PROVIDES_CAMERA_HAL),true)
-
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_C_INCLUDES += \
-    framework/native/include \
     system/media/camera/include
 
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware \
-    liblog \
-    libcamera_client \
-    libgui \
-    libhidltransport \
-    libutils \
-    android.hidl.token@1.0-utils \
-    android.hardware.graphics.bufferqueue@1.0
+    libhardware liblog libcamera_client libutils \
+    libgui android.hidl.token@1.0-utils \
 
-LOCAL_STATIC_LIBRARIES := \
-    libarect
+LOCAL_STATIC_LIBRARIES := libarect libbase
 
+LOCAL_CFLAGS += -O0
 LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE := camera.msm8960
 LOCAL_MODULE_TAGS := optional
-LOCAL_VENDOR_MODULE := true
+LOCAL_PROPRIETARY_MODULE := true
 
 include $(BUILD_SHARED_LIBRARY)
-
-endif
