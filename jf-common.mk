b0aa8ecec3be9b67f0e39d73776454dec3e82ae4
Vlad <tsvetkov_vlad@ukr.net>
2018-10-31 13:57:47 +0200

    jf-common: Rename libnfc-brcm.conf -> libnfc-nci.conf
    Change nfc.mk as well to reflect the new file name
    
    Test: compiles
    Change-Id: Ic7d88b13cd79705aced6283ca8ee5fb067611941

--- jf-common.mk
+++ jf-common.mk
@@ -166,7 +166,7 @@ PRODUCT_PACKAGES += \
     com.android.nfc_extras
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf \
+    $(LOCAL_PATH)/configs/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/configs/nfcee_access.xml:system/etc/nfcee_access.xml
 
 # OMX
