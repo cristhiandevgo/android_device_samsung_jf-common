876f62d33d8a685b7725a1f53df04d8e9590cba7
Vlad <tsvetkov_vlad@ukr.net>
2018-11-02 03:09:39 +0200

    jf-common: Import vibrator HAL

--- hidl.mk
+++ hidl.mk
@@ -89,8 +89,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator HIDL
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
+    android.hardware.vibrator@1.0-service.jf
 
 # WiFi HIDL
 PRODUCT_PACKAGES += \
